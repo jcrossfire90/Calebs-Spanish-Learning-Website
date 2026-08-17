35521e2d85519cf17b7a514c7866dc8cb342f00c
jcrossfire90 <jcrossfire90@gmail.com>
2026-08-17 00:58:31 -0500

    Google Auth Signin - Auth.js

---
+++ auth.js
@@ -0,0 +1,22 @@
+// ======================================================
+// POLYGLOT AUTHENTICATION
+// Google Sign In
+// ======================================================
+
+async function signInWithGoogle() {
+
+    const { error } = await window.polyglotSupabase.auth.signInWithOAuth({
+
+        provider: "google",
+
+        options: {
+            redirectTo: window.location.origin
+        }
+
+    });
+
+    if (error) {
+        console.error("Google Sign In Error:", error);
+    }
+
+}
